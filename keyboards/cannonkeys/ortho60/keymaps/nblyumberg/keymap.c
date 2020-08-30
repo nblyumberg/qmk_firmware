8474df25dd0dea7ff9045c7d0371e2094984073c
Nick Blyumberg <nblyumberg@gmail.com>
2020-08-30 10:32:09 -0400

    Adding my keymap

--- keyboards/cannonkeys/ortho60/keymaps/nblyumberg/keymap.c
+++ keyboards/cannonkeys/ortho60/keymaps/nblyumberg/keymap.c
@@ -24,6 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _BASE 0
 #define _RAISE 1
 #define _LOWER 2
+#define _FN 3
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -74,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_ortho_5x12( \
   _______, _______, _______,   _______, _______,  _______, _______, _______, _______, _______, _______, KC_BSPC, \
   _______, _______, _______,   _______, _______,  _______, _______, _______, _______, _______, _______, KC_DEL,  \
-  KC_DEL,  KC_LEFT,   KC_UP,   KC_DOWN,   KC_RIGHT,   LSFT(KC_9),   LSFT(KC_0),   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  _______, KC_LEFT, KC_UP  ,   KC_DOWN, KC_RIGHT, LSFT(KC_9), LSFT(KC_0),KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
   KC_LSFT, KC_F13,   KC_F14,   KC_F15,   KC_F16,  _______,  _______,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______, \
   RESET,   BL_TOGG, EEPROM_RESET, _______, _______, _______, _______, _______, KC_BSPC, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
@@ -94,7 +95,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_ortho_5x12( \
   KC_GRV,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11, \
-  KC_GRV,  KC_LBRC,    KC_RBRC,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,  \
+  KC_GRV,  _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,  \
+  KC_DEL,  KC_LEFT,   KC_UP,   KC_DOWN,   KC_RIGHT,   KC_LBRC,   KC_RBRC,   KC_MINS, KC_EQL,  _______, _______, KC_BSLS, \
+  _______, _______,   _______,   _______,   _______,  _______,  _______,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______, \
+  RGB_TOG, RGB_MOD, BL_INC,  BL_DEC,  _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+),
+
+/* Function
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_FN] = LAYOUT_ortho_5x12( \
+  KC_GRV,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11, \
+  KC_GRV,  _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,  \
   KC_DEL,  KC_LEFT,   KC_UP,   KC_DOWN,   KC_RIGHT,   KC_LBRC,   KC_RBRC,   KC_MINS, KC_EQL,  _______, _______, KC_BSLS, \
   _______, _______,   _______,   _______,   _______,  _______,  _______,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______, \
   RGB_TOG, RGB_MOD, BL_INC,  BL_DEC,  _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
@@ -130,7 +152,27 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
 	);
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    return true;
+    switch (keycode) {
+    case LOWER:
+        if (record->event.pressed) {
+            layer_on(_LOWER);
+            update_tri_layer(_LOWER,_RAISE,_FN);
+        } else {
+            layer_off(_LOWER);
+            update_tri_layer(_LOWER,_RAISE,_FN);
+        }
+        return false;
+    case RAISE:
+        if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _FN);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _FN);
+      }
+      return false;
+    }
+return true;
 }
 
 void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -143,8 +185,11 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       rgb_preview_timer = timer_read32();
       break;
+
+
   }
 # endif
+
   return;
 }
 
