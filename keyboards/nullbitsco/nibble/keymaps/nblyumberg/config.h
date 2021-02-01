6d1005a9b58515bf34d367736b0a849dedcc45ca
Nick Blyumberg <nblyumberg@gmail.com>
2021-02-01 10:52:51 -0500

    Added nblyumberg keymap to nibble

---
+++ keyboards/nullbitsco/nibble/keymaps/nblyumberg/config.h
@@ -0,0 +1,9 @@
+
+//Define a preview timeout for RGB reviews
+#define PREVIEW_TIMEOUT 5000
+#define TAPPING_TERM 200
+
+// Enable Light Layers implementation
+#define RGBLIGHT_LAYERS
+// Allow Light Layers to override RGB off configuration
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
