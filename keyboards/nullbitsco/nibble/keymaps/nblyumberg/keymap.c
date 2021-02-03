968ba7965b320ed27b658e837fafa3fa056a9d51
Nick Blyumberg <nblyumberg@gmail.com>
2021-02-03 10:29:42 -0500

    Updates to keymap

--- keyboards/nullbitsco/nibble/keymaps/nblyumberg/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/nblyumberg/keymap.c
@@ -22,7 +22,8 @@ static uint32_t rgb_preview_timer = 0;
 extern rgblight_config_t rgblight_config;
 #define _BASE 0
 #define _FN 1
-#define _ADJUST 2
+#define _MACROS 2
+#define _ADJUST 3
 
 enum custom_keycodes {
   KC_CUST = SAFE_RANGE,
@@ -30,21 +31,31 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_ansi(
-            KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_TILD,
-    TG(_ADJUST), KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, MO(_FN),
-    TG(_FN), MO(_ADJUST), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+            KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,
+    KC_MPLY, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, MO(_FN),
+    TG(_MACROS), KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
     TG(_FN), KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
-    KC_UP, KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                    MO(_FN), KC_RALT, KC_RCTL, KC_LEFT,          KC_DOWN, KC_RGHT
+    TG(_ADJUST), KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                    MO(_FN), KC_RALT, KC_RCTL, KC_LEFT,          KC_DOWN, KC_RGHT
   ),
+
   [_FN] = LAYOUT_ansi(
+             RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_DEL,  KC_INS,
+    _______, EEPROM_RESET, _______, _______, KC_UP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+    _______, _______, KC_F13, KC_F14, KC_F15, KC_F15, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+    _______, _______, _______, _______,                   _______,                   _______, _______, _______, _______,          _______, _______
+  ),
+
+  [_MACROS] = LAYOUT_ansi(
              RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_HOME,  KC_INS,
     _______, EEPROM_RESET, _______, _______, KC_UP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
     _______, _______, KC_F13, KC_F14, KC_F15, KC_F15, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
     _______, _______, _______, _______,                   _______,                   _______, _______, _______, _______,          _______, _______
   ),
+
   [_ADJUST] = LAYOUT_ansi(
-               RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_HOME,  KC_INS,
+               RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  RGB_RMOD,  RGB_MOD, KC_HOME,  KC_INS,
     RGB_TOG, EEPROM_RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
