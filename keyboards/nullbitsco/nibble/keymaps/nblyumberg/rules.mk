984ae31703dfa453d2d5204f9430d7e1ca08e3ea
Nick Blyumberg <nblyumberg@gmail.com>
2021-02-21 11:53:34 -0500

    Added tap dance in rules.mk

--- keyboards/nullbitsco/nibble/keymaps/nblyumberg/rules.mk
+++ keyboards/nullbitsco/nibble/keymaps/nblyumberg/rules.mk
@@ -1,5 +1,13 @@
+# Enable bootmagic to allow EEPROM reset with
 BOOTMAGIC_ENABLE = lite
+
+# Enable the VIA Integration
 VIA_ENABLE = yes
 LTO_ENABLE = yes
+
+# Include additional source files
 SRC += keymaps/via/nibble_encoder.c
 SRC += keymaps/via/via_extras.c
+
+# Enable Tap Dance
+TAP_DANCE = yes
